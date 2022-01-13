915e4c8bd94df75b19805565d28393c5ee30f64d
m210 <m210-2007@mail.ru>
2022-01-13 10:00:44 +0300

    Initial commit

---
+++ src/com/company/Lesson1/Bicycle.java
@@ -0,0 +1,11 @@
+package com.company.Lesson1;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
