197689b98c7ee8daa758f234711b02ea4c53aad2
m210 <m210-2007@mail.ru>
2022-01-14 15:10:37 +0300

    Car extends Bicycle

--- src/com/company/Lesson1/Car.java
+++ src/com/company/Lesson1/Car.java
@@ -1,6 +1,6 @@
 package com.company.Lesson1;
 
-public class Car extends Vehicle {
+public class Car extends Bicycle {
 
     public Car(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
@@ -10,15 +10,9 @@ public class Car extends Vehicle {
         System.out.println("Проверяем двигатель");
     }
 
-    // Можно было бы, конечно, унаследоваться от Велосипеда, тогда не пришлось бы дублировать этот код :)
-    protected void updateTyre() {
-        System.out.println("Меняем покрышку");
-    }
-
     @Override
     public void check() {
-        for(int i = 0; i < getWheelsCount(); i++)
-            updateTyre();
+        super.check();
         checkEngine();
     }
 }
